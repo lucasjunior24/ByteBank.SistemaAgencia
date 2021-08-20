ee24fd5245e2c959b098e0463b9c46c30bd12ae3
lucas souza <lukateku@gmail.com>
2021-08-20 16:00:27 -0400

    argumentos nomeados e opcionais

--- ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
+++ ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -10,15 +10,16 @@ namespace ByteBank.SistemaAgencia
         private ContaCorrente[] _itens;
         private int _proximaPosicao;
 
-        public ListaDeContaCorrente()
+        public ListaDeContaCorrente(int capacidadeInicial = 5)
         {
-            _itens = new ContaCorrente[5];
+            _itens = new ContaCorrente[capacidadeInicial];
             _proximaPosicao = 0;
         }
 
         public void Adicionar(ContaCorrente item)
         {
             VerificarCapacidade(_proximaPosicao + 1);
+
             Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
             _itens[_proximaPosicao] = item;
             _proximaPosicao++;
@@ -30,7 +31,13 @@ namespace ByteBank.SistemaAgencia
             {
                 return;
             }
-            ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+            int novoTamanho = _itens.Length * 2;
+            if(novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
 
             for (int indice = 0; indice < _itens.Length; indice++)
             {
